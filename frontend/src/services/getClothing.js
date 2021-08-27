09693ccabf337c0fbb2419dc264e8c9a191f1ee9
sampreethamith <sami0013@student.monash.edu>
2021-08-27 14:20:19 +1000

    Clothing and SPF level UI fixed

---
+++ frontend/src/services/getClothing.js
@@ -0,0 +1,10 @@
+export default function getClothing(num_of_items) {
+  const clothMetaData = [
+    "Long body coverage",
+    "White or pastel colours",
+    "UV absorbers",
+    "Moisture content",
+    "More layering Fabric",
+  ];
+  return [clothMetaData[1], clothMetaData[3], clothMetaData[2]];
+}

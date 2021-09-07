16deb39646ca156708087627f927c3c7453256bd
sampreethamith <sami0013@student.monash.edu>
2021-09-07 12:28:38 +1000

    working on fixing the production build for mapbox using carco

--- frontend/src/services/getStateGeoJson.js
+++ frontend/src/services/getStateGeoJson.js
@@ -1,7 +1,7 @@
 import http from "./httpService";
 
 const apiEndPoint =
-  "https://fix8y9br5m.execute-api.us-east-2.amazonaws.com/s3/geojson?q=state";
+  "https://fix8y9br5m.execute-api.us-east-2.amazonaws.com/s3/geojson?q=test";
 
 export default function getStateGeoJson() {
   return http.get(apiEndPoint);

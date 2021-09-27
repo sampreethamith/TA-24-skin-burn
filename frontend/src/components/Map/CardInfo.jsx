0867b5d47e3fd924e3ca269215dc0078c2319fd5
Vivek Chaudhari <vcha0018@users.noreply.github.com>
2021-09-27 22:12:38 +1000

    added scroll map feature with information.

---
+++ frontend/src/components/Map/CardInfo.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Card from "@mui/material/Card";
+
+const CardInfo = ({
+  textOneStart,
+  textOneBold,
+  textOneEnd,
+  textTwoStart,
+  textTwoBold,
+  textTwoEnd,
+  textThreeStart,
+  textThreeBold,
+  textThreeEnd,
+  visibility,
+  style,
+}) => {
+  const styleToApply =
+    style == null
+      ? {
+          display: `flex`,
+          flexDirection: `column`,
+          justifyContent: `center`,
+          alignItems: `center`,
+          height: `300%`,
+          visibility: visibility,
+        }
+      : style;
+  return (
+    <div style={styleToApply}>
+      <Card className="card-info">
+        <CardContent>
+          <Typography variant="h5" component="div">
+            {textOneStart}
+            <strong>{textOneBold}</strong>
+            {textOneEnd}
+          </Typography>
+          <Typography variant="h5" component="div">
+            {textTwoStart}
+            <strong>{textTwoBold}</strong>
+            {textTwoEnd}
+          </Typography>
+          <br />
+          <Typography variant="h6" component="div">
+            {textThreeStart}
+            <strong>{textThreeBold}</strong>
+            {textThreeEnd}{" "}
+          </Typography>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default CardInfo;

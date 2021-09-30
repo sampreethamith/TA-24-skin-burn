05a1208973ecd4540795a4f17d8fcce408ba8ccc
sampreethamith <sami0013@student.monash.edu>
2021-09-30 13:19:19 +1000

    update implementation

--- frontend/src/components/Common/CardWithBorderPrimary.jsx
+++ frontend/src/components/Common/CardWithBorderPrimary.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
-import quiz_icon from "../../images/home/quiz_icon.svg";
 
 const CardWithBorderPrimary = ({ data }) => {
   const history = useHistory();
@@ -10,11 +9,9 @@ const CardWithBorderPrimary = ({ data }) => {
       className="card-with-border-primary"
       onClick={() => history.push(data.route)}
     >
-      <img className="card-with-border-primary-img" src={quiz_icon} />
-      <p className="card-with-border-primary-title">Take a Quiz</p>
-      <p className="text-center">
-        Find more about UV index, Sun screen, and Skin cancer
-      </p>
+      <img className="card-with-border-primary-img" src={data.image} />
+      <p className="card-with-border-primary-title">{data.title}</p>
+      <p className="text-center">{data.information}</p>
     </div>
   );
 };
--- frontend/src/components/Home/HomeComponents/RouteCards.jsx
+++ frontend/src/components/Home/HomeComponents/RouteCards.jsx
@@ -1,18 +1,31 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import CardWithBorderPrimary from "../../Common/CardWithBorderPrimary";
+import quiz_icon from "../../../images/home/quiz_icon.svg";
+import aus_icon from "../../../images/home/aus_icon.svg";
+import checklist_icon from "../../../images/home/checklist_icon.svg";
 
 const RouteCards = () => {
   const cardInformation = [
     {
-      title: "Planning a trip?",
-      information: "Find high UV areas and plan your trip",
+      title: "Take a Quiz",
+      information: "Find more about UV index, Sunscreen and Skin Cancer",
+      image: quiz_icon,
+      altName: "Buld with a idea representing quiz",
+      route: "/quiz",
+    },
+    {
+      title: "Find high UV areas",
+      information: "Search for citties or states with current UV index",
+      image: aus_icon,
       altName: "Australia Map",
       route: "/map",
     },
     {
-      title: "Going Out?",
-      information: "Find right level of sunscreen and clothings to wear",
+      title: "Going Out Checklist",
+      information: "Make sure you take enough precaution before going out",
+      image: checklist_icon,
+      altName: "Check list",
       route: "/prevention/skin-burn-calculator",
     },
   ];
@@ -23,6 +36,7 @@ const RouteCards = () => {
         <div data-aos="zoom-in-up" className="route-card">
           <CardWithBorderPrimary data={cardInformation[0]} />
           <CardWithBorderPrimary data={cardInformation[1]} />
+          <CardWithBorderPrimary data={cardInformation[2]} />
         </div>
       </Container>
     </section>

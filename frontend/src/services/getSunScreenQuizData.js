1180f664f53320ccc0496dfc5c98276fb2d5cc7e
Vivek Chaudhari <vcha0018@users.noreply.github.com>
2021-10-01 20:22:16 +1000

    added quize questions. skincancer and sunscreen

--- frontend/src/services/getSunScreenQuizData.js
+++ frontend/src/services/getSunScreenQuizData.js
@@ -1,79 +1,120 @@
 export function sunScreenQuizData() {
   return [
     {
-      questionTitle: "Question SunScreen 1",
-      questionSubtitle: "What is UV stand for?",
-      imagePath: "../../images/UVSunImage.svg",
-      imageAltName: "UV",
+      questionTitle: "Do you know what the SPF value is about?",
+      questionSubtitle: "Which is locatated under label of sunscreen lotion.",
+      imagePath: "",
+      imageAltName: "SunScreen",
       options: [
         {
           optionNumber: "A",
-          optionAnswer: "Answer",
+          optionAnswer:
+            "A measure of a sunscreen's ability to prevent UVB from damaging the skin",
           correctAnswer: true,
         },
         {
           optionNumber: "B",
-          optionAnswer: "Answer",
+          optionAnswer:
+            "A measure of the number of ingredients in a sunscreen’s formula",
         },
         {
           optionNumber: "C",
-          optionAnswer: "Answer",
+          optionAnswer:
+            "A measure of what fraction of the sun’s ultraviolet rays can penetrate a fabric",
         },
         {
           optionNumber: "D",
-          optionAnswer: "Answer",
+          optionAnswer: "All of above",
         },
       ],
     },
     {
-      questionTitle: "Question SunScreen 2",
-      questionSubtitle: "What is UV stand for?",
-      imagePath: "../../images/UVSunImage.svg",
-      imageAltName: "UV",
+      questionTitle: "The best time to apply sunscreen on days is?",
+      questionSubtitle: "",
+      imagePath: "",
+      imageAltName: "SunScreen",
       options: [
         {
           optionNumber: "A",
-          optionAnswer: "Answer",
+          optionAnswer: "Immediately after going outside",
         },
         {
           optionNumber: "B",
-          optionAnswer: "Answer",
+          optionAnswer: "5-10 minutes before going outside",
         },
         {
           optionNumber: "C",
-          optionAnswer: "Answer",
+          optionAnswer: "15-30 minutes before going outside",
           correctAnswer: true,
         },
         {
           optionNumber: "D",
-          optionAnswer: "Answer",
+          optionAnswer: "Mostly, When I feel burn",
         },
       ],
     },
     {
-      questionTitle: "Question 3",
-      questionSubtitle: "What is UV stand for?",
-      imagePath: "../../images/UVSunImage.svg",
-      imageAltName: "UV",
+      questionTitle: "Who should use sunscreen?",
+      questionSubtitle: "",
+      imagePath: "",
+      imageAltName: "SunScreen",
       options: [
         {
           optionNumber: "A",
-          optionAnswer: "Answer",
+          optionAnswer: "People who have white skin",
         },
         {
           optionNumber: "B",
-          optionAnswer: "Answer",
+          optionAnswer: "People who has sunburn issue",
         },
         {
           optionNumber: "C",
-          optionAnswer: "Answer",
-          correctAnswer: true,
+          optionAnswer: "People who has long term skin cancer problem",
         },
         {
           optionNumber: "D",
-          optionAnswer: "Answer",
+          optionAnswer: "Everyone",
+          correctAnswer: true,
+        },
+      ],
+    },
+    {
+      questionTitle: "What factors you have to check before buying sunscreen?",
+      questionSubtitle: "",
+      imagePath: "",
+      imageAltName: "SunScreen",
+      options: [
+        {
+          optionNumber: "A",
+          optionAnswer: "Provides safe and effective protection against broad spectrum of ultraviolet rays",
+        },
+        {
+          optionNumber: "B",
+          optionAnswer: "With a SPF 15 (Daily use) or higher level",
+        },
+        {
+          optionNumber: "C",
+          optionAnswer: "Should be water resistant",
+        },
+        {
+          optionNumber: "D",
+          optionAnswer: "All of above",
+          correctAnswer: true,
         },
       ],
     },
   ];
+}
+
+export function getSunScreenInfo() {
+  return {
+    title: "More On Sun Screen",
+    descriptionOne:
+      "Sun Protection Factor – is a measure of a sunscreen’s ability to prevent UVB from damaging the skin.",
+    descriptionTwo:
+      "If it takes 20 minutes for your unprotected skin to start turning red, using an SPF 15 sunscreen theoretically prevents reddening 15 times longer – about five hours.",
+    descriptionThree:
+      "In reality, sunscreen wears away and loses strength after a while in the sun, so you need to reapply at least every couple of hours, and immediately after sweating or swimming.",
+    link: "/skincancer",
+  };
 }

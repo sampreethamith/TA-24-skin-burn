43cc794e130d27e174c8202a412e47604d07622a
sampreethamith <sami0013@student.monash.edu>
2021-10-03 00:57:29 +1000

    going out page 1

--- frontend/src/components/Prevention/PreventionCheckList.jsx
+++ frontend/src/components/Prevention/PreventionCheckList.jsx
@@ -1,19 +1,13 @@
 import React, { useState } from "react";
+import CardWithBorderPrimary from "../Common/CardWithBorderPrimary";
 import HandPanel from "./PreventionComponents/HandPanel";
 
 const PreventionCheckList = () => {
-  const [paleWhite, setPaleWhite] = useState(false);
-  const [White, setWhite] = useState(false);
-  const [brown, setBrown] = useState(false);
-  const [dark, setDark] = useState(false);
+  const [handSelected, setHandSelected] = useState(false);
 
   const skinColorSelected = () => {};
 
-  return (
-    <div>
-      <HandPanel />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default PreventionCheckList;

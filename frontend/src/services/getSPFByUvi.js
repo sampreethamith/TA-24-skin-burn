8da4a31b6e19abbde76a40fc3d2bd425a6c64bcd
Vivek Chaudhari <vcha0018@users.noreply.github.com>
2021-10-09 14:19:00 +1100

    uvi level bug fix.

--- frontend/src/services/getSPFByUvi.js
+++ frontend/src/services/getSPFByUvi.js
@@ -1,11 +1,11 @@
 export default function getSPFByUvi(skin_tone, uv_index) {
   const calcMetaData = {
-    uvies: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+    uvies: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
     skins: [
       {
         skin: "Type 1",
         tone: "Pale White",
-        relation: [10, 10, 26, 26, 26, 36, 36, 47, 47, 47, 57],
+        relation: [10, 10, 26, 26, 26, 36, 36, 47, 47, 47, 57, 64, 70, 79, 88],
       },
       // {
       //   skin: "Type 2",
@@ -15,7 +15,7 @@ export default function getSPFByUvi(skin_tone, uv_index) {
       {
         skin: "Type 3",
         tone: "White",
-        relation: [4, 4, 11, 11, 11, 15, 15, 20, 20, 20, 24],
+        relation: [4, 4, 11, 11, 11, 15, 15, 20, 20, 20, 24, 30, 39, 46, 57],
       },
       // {
       //   skin: "Type 4",
@@ -25,12 +25,12 @@ export default function getSPFByUvi(skin_tone, uv_index) {
       {
         skin: "Type 5",
         tone: "Brown",
-        relation: [2, 2, 4, 4, 4, 6, 6, 8, 8, 8, 10],
+        relation: [2, 2, 4, 4, 4, 6, 6, 8, 8, 8, 10, 15, 20, 24, 30],
       },
       {
         skin: "Type 6",
         tone: "Dark",
-        relation: [1, 1, 3, 3, 3, 5, 5, 6, 6, 6, 8],
+        relation: [1, 1, 3, 3, 3, 5, 5, 6, 6, 6, 8, 11, 15, 20, 24],
       },
     ],
   };
